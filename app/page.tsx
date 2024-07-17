f1c862458862b4e7b721d549af65f275d1ef1c74
infernodragon456 <vighumandy420@gmail.com>
2024-07-17 17:27:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div >
       <h1 className='h1-bold'>Next.js 13 is here</h1>
+      <h2 className='h2-bold'>Next.js 13 is here</h2>
+      <h3 className='h3-bold'>Next.js 13 is here</h3>
     </div>
   )
 }
