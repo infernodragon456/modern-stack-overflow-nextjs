59d8aa84d8bfa6f01cd972fdbc156691122159e1
infernodragon456 <vighumandy420@gmail.com>
2024-07-17 19:57:44 +0530

    add assets and favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,42 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import './globals.css'
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({
+  subsets: ['latin-ext'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin-ext'],
+  weight: [ '300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata : Metadata = {
+  title: 'Modern Stack Overflow',
+  description: 'A community driven platform for asking programming questions. Built using Next.js, and features AI driven solutions for user queries with a modern & responsive UI.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 'primary-text-gradient hover:text-primary-500'
+      }
+    }}>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
